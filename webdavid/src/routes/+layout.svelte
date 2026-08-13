b12871161b2cee59ccbbeda9d486dbf095fb9248
Francisco David Nebreda Santos <davidnebreda@studio442.website>
2026-08-13 15:52:22 +0200

    Inicio de el proyecto. creando el Layout

---
+++ webdavid/src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+	import 'normalize.css/normalize.css';
+	import 'bulma/css/bulma.min.css';
+	import 'animate.css/animate.min.css';
+	import 'fontawesome-free/css/all.min.css';
+	import '../styles.css';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
---
+++ webdavid/src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<div id="home_page" class="page">
+    Hola Mundo
+</div>
